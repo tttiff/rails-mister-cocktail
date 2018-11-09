75e681c9896f7f2af9f68dfc052c1d733c1545c8
tttiff <charles.tiffany.r@gmail.com>
2018-11-09 10:39:08 +0000

    done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,4 +1,46 @@
 class DosesController < ApplicationController
   def index
+    @doses = Dose.all
+  end
+
+  def show
+    @dose = Dose.find(params[:id])
+  end
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params[dose_params])
+    if @dose.save
+      redirect_to dose_path(@dose.id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @dose = Dose.find(params[:id])
+  end
+
+  def update
+    if @dose.update(dose_params)
+      redirect_to doses_path(@dose)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to dose_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
   end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,6 @@
 class Ingredient < ApplicationRecord
+  has_many :doses
+  has_many :cocktails
+
+  validates :name, presence: true, uniqueness: true
 end
